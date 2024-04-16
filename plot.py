3bc57ddbfc14d7cf3cf3917357bc9f20463dae76
alex-a-avicena <156715519+alex-a-avicena@users.noreply.github.com>
2024-04-16 10:35:09 -0700

    Add parula, turbo, abyss colormaps from MATLAB, add custom grayblue colormap

---
+++ plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+import matplotlib as mpl
+from matplotlib.colors import rgb2hex, LinearSegmentedColormap
+import matplotlib
+
+
